ae2dbf0a47a0bd14c4d3da3fcee12380dd572327
theogguu <stevengu.is.not@gmail.com>
2023-11-13 17:47:20 -0600

    Static ProfileForm/Event Creation stuff added

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -68,12 +68,68 @@ const ProfileForm = ({ onProfileSubmit, user }) => {
     setImageFile(null);
   };
 
+
+  // <Card.Img variant="top" src={person.image} />
+//   <Card.Body>
+//   <Card.Title>{person.name}</Card.Title>
+
+//   <Card.Text>Event: Go to work</Card.Text>
+//   <Card.Text>Mode of transportation: SUV</Card.Text>
+//   <Card.Text>Max number of people: 5</Card.Text>
+//   <Card.Text>Datetime: 11/10/23</Card.Text>
+//   <Card.Text>Recurring: Yes</Card.Text>
+//   <Card.Text>Notes: May have dog hair. Non smoking</Card.Text>
+//   <a href={`mailto:${person.email}`}>
+//     <Button variant="primary">Contact</Button>
+//   </a>
+// </Card.Body>
+  // 
   return (
     <div className='profileForm'>
       <Form onSubmit={handleSubmit}>
         <Form.Group>
-          <Form.Label>Skills Have</Form.Label>
-          {skillsHaveFields.map((field, index) => (
+          <Form.Label>Event Name</Form.Label>
+          <Form.Control placeholder="grocery run..." required type="text" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Mode of transportation</Form.Label>
+          <Form.Control placeholder="SUV, Sedan, ..." required type="text" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Max number of people</Form.Label>
+          <Form.Control required type="number" min="1"/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Datetime</Form.Label>
+          <Form.Control required type="date" />
+          <Form.Control required type="time" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Recurring</Form.Label>
+          <Form.Control
+                  as="select"
+                  // value={field.level}
+                  // onChange={(e) => handleSkillsHaveChange(index, 'level', e.target.value)}
+                >
+                  <option value="beginner">No</option>
+                  <option value="intermediate">Yes</option>
+            </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Additional Notes</Form.Label>
+          <Form.Control placeholder="e.g., may have dog hair" type="text" />
+        </Form.Group>
+
+        <Form.Group className="form-group">
+          <Form.Label>Profile Picture</Form.Label>
+          <Form.Control type="file" onChange={handleFileChange} accept=".jpg,.jpeg,.png" />
+        </Form.Group>
+
+        <Button type="submit">Create Profile</Button>
+      </Form>
+
+          {/* {skillsHaveFields.map((field, index) => (
             <div className="row">
               <div key={index} className="input-skills-have">
                 <div className="col-7">
@@ -98,24 +154,16 @@ const ProfileForm = ({ onProfileSubmit, user }) => {
                 <Button variant="danger" className="remove-skills-button" onClick={() => removeSkillsHaveField(index)}>-</Button>
               </div>
             </div>
-          ))}
+          ))} 
           <Button variant="success" className="add-skills-button" onClick={addSkillsHaveField}>+ Add Skill</Button>
-        </Form.Group>
+          </Form.Group> */}    
+{/* 
+        <Form.Group>
+          <Form.Label>Card Color</Form.Label>
+          <Form.Control required type="color" />
+        </Form.Group>     */}
 
-
-        <Form.Group className="form-group">
-          <Form.Label>Skills Want (comma separated)</Form.Label>
-          <Form.Control placeholder="coding,baking,etc..." required type="text" value={skillsWant} onChange={e => setSkillsWant(e.target.value)} />
-        </Form.Group>
         
-
-        <Form.Group className="form-group">
-          <Form.Label>Profile Picture</Form.Label>
-          <Form.Control type="file" onChange={handleFileChange} accept=".jpg,.jpeg,.png" />
-        </Form.Group>
-
-        <Button type="submit">Create Profile</Button>
-      </Form>
     </div>
   );
 };
